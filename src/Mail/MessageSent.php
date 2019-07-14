7339df6aaf8ccf3bb119f7295dcb430b328feeb9
Robert Garrigos <robert@garrigos.cat>
2019-07-14 17:26:20 +0200

    Apply fixes from StyleCI (#2)

--- src/Mail/MessageSent.php
+++ src/Mail/MessageSent.php
@@ -5,7 +5,6 @@ namespace Robertgarrigos\Contact\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class MessageSent extends Mailable
 {
@@ -14,7 +13,6 @@ class MessageSent extends Mailable
     public $name;
     public $email;
     public $text;
-
 
     /**
      * Create a new message instance.
